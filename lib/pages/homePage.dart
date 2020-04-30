8b0a671d2dec51e407c5fae8d36d62f6600b3847
Luis <cespop@gmail.com>
2020-04-30 17:42:29 -0300

    Iniciando com formulários

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -7,27 +7,38 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Meus feeds'),
-        automaticallyImplyLeading: false,
-      ),
-      body: Center(
-        child: RaisedButton(
-          child: Text('Avançar'),
-          onPressed: () {
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => ArticlesPage(
-                    feed: 'http://www.fatosystem.com.br/',
-                  ),
-                ));
-          },
+        appBar: AppBar(
+          title: Text('Meus feeds'),
+          automaticallyImplyLeading: false,
         ),
-      ),
-    );
+        body: Padding(
+          padding: EdgeInsets.all(20),
+          child: Form(
+            key: this._formKey,
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  keyboardType: TextInputType.url,
+                  decoration: InputDecoration(
+                    labelText: "Link do rss",
+                  ),
+                ),
+                RaisedButton(
+                  child: Text('Cadastrar'),
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                  onPressed: () {
+                    print('adicionar');
+                  },
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
