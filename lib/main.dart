5f3026b1568da21de8111315d6fe0d7c602dce98
Luis <cespop@gmail.com>
2020-05-04 18:55:06 -0300

    Abrindo link no navegador padrão

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Feeds Reader',
       theme: ThemeData(
         primarySwatch: Colors.blue,
--- lib/pages/articles_page.dart
+++ lib/pages/articles_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../data/getFeedData.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ArticlesPage extends StatefulWidget {
   final String feed;
@@ -22,30 +23,37 @@ class _ArticlesPageState extends State<ArticlesPage> {
     articles = rss.read(url: this.feed);
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Lista de Artigos'),
-        ),
-        body: FutureBuilder(
-          future: articles,
-          builder: (context, snapshot) {
-            switch (snapshot.connectionState) {
-              case ConnectionState.none:
-              case ConnectionState.waiting:
-                return messageTemplate(
-                    title: 'Carregando ...', icon: Icons.refresh);
-              default:
-                if (snapshot.hasError) {
-                  messageTemplate(
-                      title: 'Não foi possível carregar ...',
-                      icon: Icons.error);
-                }
-                return listArticles(snapshot.data);
-            }
-          },
-        ));
+      appBar: AppBar(
+        title: Text('Lista de Artigos'),
+        leading: Icon(Icons.rss_feed),
+      ),
+      body: FutureBuilder(
+        future: articles,
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return messageTemplate(
+                  title: 'Carregando ...', icon: Icons.refresh);
+            default:
+              if (snapshot.hasError) {
+                messageTemplate(
+                    title: 'Não foi possível carregar ...', icon: Icons.error);
+              }
+              return listArticles(snapshot.data);
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.arrow_back),
+        onPressed: () {
+          Navigator.pop(context);
+        },
+      ),
+    );
   }
 
-  //Método
+  //Métodos:
   messageTemplate({String title, IconData icon}) {
     return Center(
         child: Column(
@@ -67,12 +75,28 @@ class _ArticlesPageState extends State<ArticlesPage> {
                     title: Text(articles[index]['title']),
                     subtitle: Text(articles[index]['link']),
                     leading: Icon(Icons.open_in_browser),
-                    onTap: () {
-                      print(articles[index]['link']);
+                    onTap: () async {
+                      //openURL(articles[index]['link']);
+                      String url = articles[index]['link'];
+                      canLaunch(url).then((isConfirmed) {
+                        if (isConfirmed) {
+                          launch(url);
+                        } else {
+                          throw 'Could not launch $url';
+                        }
+                      });
                     },
                   );
                 }))
       ],
     );
+  }
+
+  openURL(url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
