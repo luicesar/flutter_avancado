cdc9ecf5b2dc121384ecd3ea35ca5474f7aee56e
Luis <cespop@gmail.com>
2020-05-04 17:48:51 -0300

    Conhecendo o future builder

--- lib/pages/articles_page.dart
+++ lib/pages/articles_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../data/getFeedData.dart';
 
 class ArticlesPage extends StatefulWidget {
   final String feed;
@@ -11,22 +12,47 @@ class ArticlesPage extends StatefulWidget {
 
 class _ArticlesPageState extends State<ArticlesPage> {
   final String feed;
+  Future<List> articles;
+
   _ArticlesPageState(this.feed);
 
   @override
   Widget build(BuildContext context) {
+    GetFeedData rss = new GetFeedData();
+    articles = rss.read(url: this.feed);
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Lista de Artigos'),
-      ),
-      body: Center(
-        child: RaisedButton(
-          child: Text(this.feed),
-          onPressed: () {
-            //Navigator.pushNamed(context, '/');
-          },
+        appBar: AppBar(
+          title: Text('Lista de Artigos'),
         ),
-      ),
-    );
+        body: FutureBuilder(
+          future: articles,
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+              case ConnectionState.waiting:
+                return messageTemplate(
+                    title: 'Carregando ...', icon: Icons.refresh);
+              default:
+                if (snapshot.hasError) {
+                  messageTemplate(
+                      title: 'Não foi possível carregar ...',
+                      icon: Icons.error);
+                }
+                return messageTemplate(title: 'Carregado', icon: Icons.done);
+            }
+          },
+        ));
+  }
+
+  //Método
+  messageTemplate({String title, IconData icon}) {
+    return Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+          Icon(icon, size: 50),
+          Text(title),
+        ]));
   }
 }
